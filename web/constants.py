145aac1c64111db313a9516f9c8c2d597f2776ad
pansulbhatt <pansul@xiaoyus-mbp.home>
2020-04-14 13:47:44 -0400

    Added S3 connection

---
+++ web/constants.py
@@ -0,0 +1,3 @@
+S3_BUCKET_NAME = 'uploads-results-prod.zappotrack.com'
+DEFAULT_DB_HOST = 'localhost'
+DEFAULT_DB_PORT = 3306
--- web/s3_connection.py
+++ web/s3_connection.py
@@ -1,8 +1,35 @@
+import json
 import boto3
 
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
+class S3Interface():
+    """
+    S3Interface class is the key to load your modules or objects
+    from S3 to the code. This class's functionality is enabled
+    using s3_client and s3_resource class variables. These would be shared among
+    multiple objects.
+    """
+    s3_client = boto3.client('s3')
+    s3_resource = boto3.resource('s3')
+
+    def __init__(self, bucket_name):
+        """
+        Set the bucket name using the constructor
+        """
+        self.bucket_name = bucket_name
+
+    def get_all_buckets(self):
+        """
+        Fetches all the bucket names which are present in the account
+        """
+        return [b.name for b in self.s3_resource.buckets.all()]
 
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+    def get_file(self, file_name):
+        """
+        Returns the file. Do note that the expectation here is that the
+        file which is being returned is a json.
+        """
+        data = self.s3_client.get_object(Bucket=self.bucket_name, Key=file_name)
+        contents = data['Body'].read().decode('utf-8')
+        return json.loads(contents)
+
