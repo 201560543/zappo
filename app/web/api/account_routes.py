e543f72ed8e1e8432ea62c5ab117fb4fce4a8b73
pansulbhatt <pansul@xiaoyus-mbp.home>
2020-05-01 19:19:06 -0400

    Added utils for error handling

--- app/web/api/account_routes.py
+++ app/web/api/account_routes.py
@@ -1,25 +1,37 @@
+import json
+import request
 from . import account
 from web.models.account import Account
-from web.api.api_utils import converter
+from web.api.api_utils import converter, exception_handler
 from web.database import db
 from flask import current_app
-import json
+
+@account.app_errorhandler(404)
+def page_not_found(e):
+    current_app.logger.warn(f'404 not found')
+    current_app.logger.warn(e)
+    return 'Account API not Found', 404
+
+@account.app_errorhandler(500)
+def server_error(e):
+    current_app.logger.warn('500 internal server error')
+    current_app.logger.warn(e)
+    return '500 Internal Server Error', 500
 
 @account.route('/', methods=['GET'])
+@exception_handler(custom_msg='Issues in fetching')
 def get_all_accounts(return_json=True):
-    # TO DO: Add error handling
     results = db.session.query(Account).all()
     result_dicts = [acnt.as_dict() for acnt in results]
     current_app.logger.info(result_dicts)
-    
     if return_json == True:
         return json.dumps(result_dicts, default=converter)
     else:
         return result_dicts
 
-@account.route('/<account_number>', methods=['GET'])
+@account.route('/<int:account_number>/', methods=['GET'])
+@exception_handler()
 def get_account_by_account_number(account_number, return_json=True):
-    # TO DO: Add error handling
     results = db.session.query(Account).filter_by(account_number=account_number).all()
     result_dicts = [acnt.as_dict() for acnt in results]
     current_app.logger.info(result_dicts)
--- app/web/api/api_utils.py
+++ app/web/api/api_utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime, date
+from functools import wraps
+from flask import current_app, abort
 
 def converter(obj):
     """
@@ -8,3 +10,33 @@ def converter(obj):
         return obj.__str__()
     elif isinstance(obj, date):
         return obj.__str__()
+
+
+def exception_handler(**kw):
+    """
+    Function to handle all code errors and logs for us. This function also takes
+    keyword arguments. If you want to add more arguments here then make sure to check for
+    them as show below:
+    Arguments:
+    kw: (keyword argumets) {
+        'custom_msg': ''
+    }
+    """
+    custom_msg = kw.get('custom_msg', 'Found an exception while making a request')
+    def handler(func):
+        """
+        Main function wrapper
+        """
+        @wraps(func)
+        def inner(*args, **kwargs):
+            """
+            Inner function to log all issues
+            """
+            try:
+                return func(*args, **kwargs)
+            except Exception as exc:
+                current_app.logger.warn(custom_msg)
+                current_app.logger.warn(exc)
+                abort(400, description='Kindly contact your dev team to handle this error')
+        return inner
+    return handler
