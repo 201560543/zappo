1e6aaa6e1148c3ad9a781953de3faa422441ba4a
pansulbhatt <pansul@xiaoyus-mbp.home>
2020-05-02 16:00:08 -0400

    API error handler

--- app/web/api/supplier_routes.py
+++ app/web/api/supplier_routes.py
@@ -5,18 +5,6 @@ from web.api.api_utils import converter, exception_handler
 from web.database import db
 from flask import current_app, request
 
-@supplier.app_errorhandler(404)
-def page_not_found(e):
-    current_app.logger.warn(f'404 not found')
-    current_app.logger.warn(e)
-    return 'Supplier API not Found', 404
-
-@supplier.app_errorhandler(500)
-def server_error(e):
-    current_app.logger.warn('500 internal server error')
-    current_app.logger.warn(e)
-    return '500 Internal Server Error', 500
-
 @supplier.route('/', methods=['GET'])
 @exception_handler(custom_msg="Issues in fetching all suppliers")
 def get_all_suppliers(return_json=True):
