9c2bd892d6f98e23eb904ec68d4c0a2d966438c9
pansulbhatt <pansul@xiaoyus-mbp.home>
2020-05-07 21:09:24 -0400

    updating circleci

---
+++ data/sql_data/dummy.sql
@@ -0,0 +1,331 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `zappo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `zappo`;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `account` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `organization_id` int(10) unsigned NOT NULL COMMENT 'An organization can have one or multimple account accross time. Account could be closed. Another one opened.',
+  `account_number` char(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Possible account number (auto-generated)',
+  `account_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `is_active` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT 'Allow track if account is active or de-activated (non payment). Inactive account cannot login.',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `timezone_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Timezone of the account.',
+  PRIMARY KEY (`id`),
+  KEY `organization_id` (`organization_id`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `organization_id` int(10) unsigned NOT NULL,
+  `address_type_id` tinyint(3) unsigned NOT NULL COMMENT 'Shipping, Business etc ...',
+  `country_id` smallint(5) unsigned NOT NULL,
+  `address_name` varchar(100) NOT NULL,
+  `address_name_additional` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(20) NOT NULL,
+  `city_name` varchar(65) NOT NULL,
+  `is_active` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT 'Business could move to another address, address could change.',
+  `from_date` date NOT NULL COMMENT 'Active Address From Date',
+  `thru_date` date DEFAULT NULL COMMENT 'Active at this address till.',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'Soft delete.',
+  PRIMARY KEY (`id`),
+  KEY `fk_address_address_type` (`address_type_id`),
+  KEY `fk_address_country` (`country_id`),
+  CONSTRAINT `fk_address_address_type` FOREIGN KEY (`address_type_id`) REFERENCES `address_type` (`id`),
+  CONSTRAINT `fk_address_country` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COMMENT='Organization Address, Restaurant  Address, Invoice Ship To Address, etc ..';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `address_type`
+--
+
+DROP TABLE IF EXISTS `address_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address_type` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `address_type_name` varchar(35) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COMMENT='Business Address,  Invoice Ship To Address,  Billing Address';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address_type`
+--
+
+LOCK TABLES `address_type` WRITE;
+/*!40000 ALTER TABLE `address_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `country` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `country_name` varchar(100) NOT NULL,
+  `country_code` char(3) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_code` (`country_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=249 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `country`
+--
+
+LOCK TABLES `country` WRITE;
+/*!40000 ALTER TABLE `country` DISABLE KEYS */;
+INSERT INTO `country` VALUES (1,'India','IND','2020-04-27 20:01:07','2020-04-27 20:01:07'),(2,'Philippines','PHI','2020-04-27 20:02:11','2020-04-27 20:02:11');
+/*!40000 ALTER TABLE `country` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `country_import`
+--
+
+DROP TABLE IF EXISTS `country_import`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `country_import` (
+  `SK_Country` int(11) DEFAULT NULL,
+  `CountryName` varchar(255) DEFAULT NULL,
+  `Alpha3Code` char(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `country_import`
+--
+
+LOCK TABLES `country_import` WRITE;
+/*!40000 ALTER TABLE `country_import` DISABLE KEYS */;
+/*!40000 ALTER TABLE `country_import` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `industry`
+--
+
+DROP TABLE IF EXISTS `industry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `industry` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `industry_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `industry`
+--
+
+LOCK TABLES `industry` WRITE;
+/*!40000 ALTER TABLE `industry` DISABLE KEYS */;
+/*!40000 ALTER TABLE `industry` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `organization` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_organization_id` int(10) unsigned DEFAULT NULL,
+  `organization_type_id` smallint(5) unsigned NOT NULL COMMENT 'Zappo, Supplier, Restaurant (other industry)',
+  `organization_number` varchar(32) NOT NULL DEFAULT '',
+  `industry_id` smallint(5) unsigned NOT NULL,
+  `organization_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `website_url` varchar(65) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `timezone_name` varchar(50) NOT NULL DEFAULT 'Pacific Daylight Time/Vancouver',
+  PRIMARY KEY (`id`),
+  KEY `fk_organization_organization_type` (`organization_type_id`),
+  KEY `fk_organization_industry` (`industry_id`),
+  KEY `idx_org_number` (`organization_number`),
+  KEY `idx_parent_id` (`parent_organization_id`),
+  CONSTRAINT `fk_organization_industry` FOREIGN KEY (`industry_id`) REFERENCES `industry` (`id`),
+  CONSTRAINT `fk_organization_organization_type` FOREIGN KEY (`organization_type_id`) REFERENCES `organization_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization`
+--
+
+LOCK TABLES `organization` WRITE;
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization_type`
+--
+
+DROP TABLE IF EXISTS `organization_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `organization_type` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `organization_type_name` varchar(50) NOT NULL COMMENT 'Zappo, Supplier, Restaurant, Other (industries)',
+  `is_active` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT 'Flag that states if ooganization type is active in the UI. (selectable)',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COMMENT='Restaurant, Supplier, zappo_track, Accounting, ...';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization_type`
+--
+
+LOCK TABLES `organization_type` WRITE;
+/*!40000 ALTER TABLE `organization_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `organization_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restaurant`
+--
+
+DROP TABLE IF EXISTS `restaurant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `restaurant` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `organization_id` int(10) unsigned NOT NULL,
+  `restaurant_name` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+LOCK TABLES `restaurant` WRITE;
+/*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `organization_id` int(10) unsigned NOT NULL,
+  `supplier_name` varchar(100) NOT NULL,
+  `business_name` varchar(100) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `logo_path` varchar(100) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supplier`
+--
+
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `zappo_track`
+--
+
+DROP TABLE IF EXISTS `zappo_track`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `zappo_track` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `oganization_id` int(10) unsigned NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `zappo_track`
+--
+
+LOCK TABLES `zappo_track` WRITE;
+/*!40000 ALTER TABLE `zappo_track` DISABLE KEYS */;
+/*!40000 ALTER TABLE `zappo_track` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-07 21:05:16
